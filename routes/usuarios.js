c27be1b79d2bc56884cea1aadafc12092582e331
Ramon Raniere <101540300+RRaniere@users.noreply.github.com>
2022-06-22 00:55:10 -0400

    Add files via upload
    now it is possible to register and login, already with JWT token

---
+++ routes/usuarios.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const mysql = require('../mysql').pool
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+router.post('/cadastro', (req,res,next) => { 
+    mysql.getConnection((err,conn) => {
+        if (err) {return res.status(500).send({error: error})}
+        conn.query('SELECT * FROM usuarios WHERE email = ?', [req.body.email], (error, result) => { 
+            if (error) {return res.status(500).send({error:error})}
+            if (result.length > 0) {
+                res.status(409).send({mensagem: 'Usuário já cadastrado'})
+            } else {
+                bcrypt.hash(req.body.senha, 10, (errbcypt, hash) => { 
+                    if (errbcypt) {return res.status(500).send({error: errbcypt})}
+                    conn.query('INSERT INTO usuarios (email, senha) VALUES (?,?)', 
+                    [req.body.email, hash],
+                    (error, results) => {
+                        conn.release()
+                        if (error) {return res.status(500).send({error: error})}
+                        
+                        response = {
+                        mensagem : 'Usuário crido com sucesso',
+                        usuarioCriado: { 
+                            id_usuario: results.insertId,
+                            email : req.body.email
+                        }
+                    }
+                        return res.status(201).send({ response })
+                    })
+                })
+            }
+        })  
+    })
+})
+
+router.post('/login', (req,res,next) => { 
+    mysql.getConnection((error, conn) => { 
+        const query = 'SELECT * FROM usuarios WHERE email =?';
+        conn.query(query,[req.body.email], (error, results,fiels) => { 
+            conn.release()
+            if (error) {return res.status(500).send({ error: error})}
+            if (results.length < 1) { 
+                return res.status(401).send({ mensagem: 'Falha na autenticação'})
+            }
+            bcrypt.compare(req.body.senha, results[0].senha, (err, result) => { 
+                if (err) { 
+                    return res.status(404).send({mensagem: 'Falha na autenticação'})
+                }
+                if( result) { 
+                    let token = jwt.sign({ 
+                        id_usuario: results[0].id_usuario,
+                        email:results[0].email
+                    },'segredo', {expiresIn: "1h"})
+
+                    return res.status(200).send({
+                        
+                        mensagem: 'Autenticado com sucesso',
+                        token: token
+
+                    })
+
+                }
+                return res.status(404).send({mensagem: 'Falha na autenticação'})
+            })
+        })
+    })
+})
+
+
+module.exports = router
